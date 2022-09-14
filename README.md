7ea1f85e78bc3f41451834d3d0ee0794a7f0b616
Rui <ruiseixas@yahoo.com>
2022-09-14 17:28:46 +0100

    hello_world

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # libraries_implementations
  Implementation of Libraries ready to be used
+
+HOW-TO (Visual Studio Code):
+	1. Create a folder "repositories" on the git folder
+	2. Create the folders "arduino" and "computer" under it
+	3. Clone a given repository into the respective folder (github Desktop via URL)
+	4. Copy the root files .cpp and .h into the include folder of the project
---
+++ projects/hellow_world/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/**
+ * @brief An example program. This program demonstrates simple vextor storage and element iteration.
+ *        A welcome string is output to the user.
+ * 
+ * @return Returns 0 when execution completes successfully.
+ */
+
+int main()
+{
+	std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+	for (const std::string& word : msg)
+	{
+		std::cout << word << " ";
+	}
+	std::cout << std::endl;
+
+}
